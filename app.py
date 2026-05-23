c3dd49e06c8940c0906a441d236e49c33be299b7
hyzhao1222 <hyzhao1222@gmail.com>
2026-05-23 12:11:37 +0800

    improving page

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    name  = "Alex Zhao"
+    return render_template('index.html', name=name)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 app.run(debug=True)
